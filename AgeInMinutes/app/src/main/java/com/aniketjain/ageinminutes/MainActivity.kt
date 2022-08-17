dc5b0360fb4bf2ac00ffa19f3579ac67997a9fb2
aniketjain8441 <aniketjain8441@gmail.com>
2022-08-18 02:39:54 +0530

    master

--- AgeInMinutes/app/src/main/java/com/aniketjain/ageinminutes/MainActivity.kt
+++ AgeInMinutes/app/src/main/java/com/aniketjain/ageinminutes/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.aniketjain.ageinminutes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aniketjain.ageinminutes.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // listeners
+        listeners()
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun listeners() {
+        binding.datePickBtn.setOnClickListener {
+//            Log.i("btn", "clicked")
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { _, year, month, day ->
+
+                    // 1st tv setUp
+                    val selectedDate = "$day/${month + 1}/$year"
+                    binding.dateTv.text = selectedDate
+
+                    // 2nd tv setUp
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                    val date = sdf.parse(selectedDate)
+                    val selectedDateInMin = date!!.time / (1000 * 60)
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    val currentDateInMin = currentDate!!.time / (1000 * 60)
+
+                    binding.minTv.text = (currentDateInMin - selectedDateInMin).toString()
+
+                }, year, month, day
+            ).show()
+        }
     }
 }
