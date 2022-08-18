de57a8f7f8ff2c480aa8d48d398414fd0cf7c40b
aniketjain8441 <aniketjain8441@gmail.com>
2022-08-18 20:05:38 +0530

    master

--- Calculator/app/src/main/java/com/aniketjain/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/aniketjain/calculator/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.aniketjain.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.aniketjain.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var lastNumeric: Boolean = false
+    private var lastDot: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+
+    fun onDigit(view: View) {
+        binding.inputTv.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View) {
+        binding.inputTv.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimal(view: View) {
+        if (lastNumeric && !lastDot) {
+            binding.inputTv.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
